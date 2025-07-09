d4730eff884c0603d2c5d7cc34e1bcebcf3b5985
Deeptanshu Shukla <deeptanshushuklaji@gmail.com>
2025-07-10 03:09:22 +0530

    Removed unused codes from the project.

--- composeApp/src/commonMain/kotlin/com/potato/split/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.potato.split
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/potato/split/domain/useCase/expense/AddExpenseUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.potato.split.domain.useCase.expense
-
-//class AddExpenseUseCase(
-//    private val expenseRepository: ExpenseRepository
-//) {
-//    suspend operator fun invoke(groupId: String, expense: Expense): Result<Unit> =
-//        expenseRepository.addExpense(groupId, expense)
-//}
